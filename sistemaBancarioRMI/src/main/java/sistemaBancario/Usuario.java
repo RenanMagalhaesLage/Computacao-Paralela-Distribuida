faad271cb9552e34968a1c591727149c6ed32f92
Renan Magalhães Lage <89847080+RenanMagalhaesLage@users.noreply.github.com>
2023-12-01 10:54:19 -0300

    Add files via upload

---
+++ sistemaBancarioRMI/src/main/java/sistemaBancario/Usuario.java
@@ -0,0 +1,27 @@
+package sistemaBancario;
+
+public class Usuario {
+    private int id;
+    private String nome;
+    private String sobrenome;
+    double saldo;
+
+    public Usuario(int id, String nome, String sobrenome, double saldo) {
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.saldo = saldo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+}
